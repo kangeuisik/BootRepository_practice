92813e6eb0975aafb7c51f228cd02bb186c76930
kang <leekwanghyup@naver.com>
2023-05-29 19:01:10 +0900

    영속성전이

--- Chapter05/src/main/java/com/rubypaper/domain/Board.java
+++ Chapter05/src/main/java/com/rubypaper/domain/Board.java
@@ -35,6 +35,15 @@ public class Board {
 	@JoinColumn(name = "MEMBER_ID", nullable = false) //관계를 맺을 컬럼
 	private Member member;
 	
+	//member클래스에서 cascadeType.ALL로 설정할 경우 회원객체가 영속화되거나 수정,삭제 될때
+	//회원과 관련된 게시판도 함께 반영됨
+	//회원이 소유한 게시글도 자동으로 수정삭제 될수 있도록 board클래스에 setMember()새로이 추가하자
+	
+	public void setMember(Member member) {
+		this.member = member;
+		member.getPersonBoardList().add(this);
+	}
+	
 	//@ManyToOne에서 사용할 수 있는 속성
 	//optional : 연관되 엔티티가 반드시 있어야 하는지의 여부 결정 false로 설정하면 항상 있어야함
 	//fetch : 글로버러 페치전략 설정
--- Chapter05/src/test/java/com/rubypaper/RelationMappingTest.java
+++ Chapter05/src/test/java/com/rubypaper/RelationMappingTest.java
@@ -25,56 +25,56 @@ public class RelationMappingTest {
 	private MemberRepository memberRep;
 	
 	
-//	@Test
-//	public void testManyToOneInsert() {
-//		Member member1 = new Member();
-//		member1.setId("회원1");
-//		member1.setName("손흥민");
-//		member1.setPassword("1234");
-//		member1.setRole("User");
-//		
+	@Test
+	public void testManyToOneInsert() {
+		Member member1 = new Member();
+		member1.setId("회원1");
+		member1.setName("손흥민");
+		member1.setPassword("1234");
+		member1.setRole("User");
+		
 //		memberRep.save(member1);
-//		
-//		Member member2 = new Member();
-//		member2.setId("회원2");
-//		member2.setName("김민재");
-//		member2.setPassword("1234");
-//		member2.setRole("관리자");
-//		
+		
+		Member member2 = new Member();
+		member2.setId("회원2");
+		member2.setName("김민재");
+		member2.setPassword("1234");
+		member2.setRole("관리자");
+		
 //		memberRep.save(member2);
-//		
-//		for (int i = 1; i <= 5; i++) {
-//			Board board = new Board();
-//			board.setMember(member1);
-//			board.setTitle("흥민이가 등록한 글" + i);
-//			board.setContent("흥민이가 등록한 글 내용"+i);
-//			board.setCreateDate(new Date());
-//			board.setCnt(0L);
-//			
+		
+		for (int i = 1; i <= 5; i++) {
+			Board board = new Board();
+			board.setMember(member1);
+			board.setTitle("흥민이가 등록한 글" + i);
+			board.setContent("흥민이가 등록한 글 내용"+i);
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+			
 //			boardRep.save(board);
-//		}
-//		for (int i = 1; i <= 5; i++) {
-//			Board board = new Board();
-//			board.setMember(member2);
-//			board.setTitle("민재가 등록한 글" + i);
-//			board.setContent("민재가 등록한 글 내용"+i);
-//			board.setCreateDate(new Date());
-//			board.setCnt(0L);
-//			
+		}
+		for (int i = 1; i <= 5; i++) {
+			Board board = new Board();
+			board.setMember(member2);
+			board.setTitle("민재가 등록한 글" + i);
+			board.setContent("민재가 등록한 글 내용"+i);
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+			
 //			boardRep.save(board);
-//		}
-//	}
-//	@Test //글 조회 검색 테스트
-//	public void testManyToOneSelect() {
-//		Board board = boardRep.findById(5L).get();
-//		
-//		System.out.println("[ "+board.getSeq()+"번 게시글 정보 ]");
-//		System.out.println("제목 : " + board.getTitle());
-//		System.out.println("내용 : " + board.getContent());
-//		System.out.println("작성자 : " + board.getWriter());
-//		System.out.println("작성일 : " + board.getCreateDate());
-//		System.out.println("작성자 권한 : " + board.getMember().getRole());
-//	}
+		}
+	}
+	@Test //글 조회 검색 테스트
+	public void testManyToOneSelect() {
+		Board board = boardRep.findById(5L).get();
+		
+		System.out.println("[ "+board.getSeq()+"번 게시글 정보 ]");
+		System.out.println("제목 : " + board.getTitle());
+		System.out.println("내용 : " + board.getContent());
+		System.out.println("작성자 : " + board.getWriter());
+		System.out.println("작성일 : " + board.getCreateDate());
+		System.out.println("작성자 권한 : " + board.getMember().getRole());
+	}
 
 	@Test
 	public void testTwoWayMapping() {
